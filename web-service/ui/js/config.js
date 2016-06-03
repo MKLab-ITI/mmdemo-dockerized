64028ce16a8378fc86310bc2abcec5337920a688
laaposto <laaposto@gmail.com>
2016-06-03 11:06:47 +0300

    Advanced User Search Added

--- web-service/ui/js/config.js
+++ web-service/ui/js/config.js
@@ -1,4 +1,5 @@
-var api_folder="/api/";
+//var api_folder="/api/";
+var api_folder = "http://160.40.50.207/STEPAPI/";
 /*Don't change it unless you really know what you are doing*/
 
 var project_logo = "";

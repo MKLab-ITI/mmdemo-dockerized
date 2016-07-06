74e3bbbf676ecd6862e71a7c4e2739bcee2804e3
manosetro <manosetro@iti.gr>
2016-07-06 17:04:41 +0300

    fix rockmongo hostname

--- web-service/rockmongo/config.php
+++ web-service/rockmongo/config.php
@@ -5,7 +5,7 @@
  * Defining default options and server configuration
  * @package rockmongo
  */
- 
+
 $MONGO = array();
 $MONGO["features"]["log_query"] = "on";//log queries
 $MONGO["features"]["theme"] = "default";//theme
@@ -15,12 +15,12 @@ $i = 0;
 
 /**
 * Configuration of MongoDB servers
-* 
+*
 * @see more details at http://rockmongo.com/wiki/configuration?lang=en_us
 */
 $MONGO["servers"][$i]["mongo_name"] = "Localhost";//mongo server name
 //$MONGO["servers"][$i]["mongo_sock"] = "/var/run/mongo.sock";//mongo socket path (instead of host and port)
-$MONGO["servers"][$i]["mongo_host"] = "127.0.0.1";//mongo host
+$MONGO["servers"][$i]["mongo_host"] = "mongodb";//mongo host
 $MONGO["servers"][$i]["mongo_port"] = "27017";//mongo port
 $MONGO["servers"][$i]["mongo_timeout"] = 0;//mongo connection timeout
 //$MONGO["servers"][$i]["mongo_db"] = "MONGO_DATABASE";//default mongo db to connect, works only if mongo_auth=false

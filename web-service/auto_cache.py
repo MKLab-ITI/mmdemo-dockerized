66a7fdefab0b6a8aabf3d19fc8fd6b58c34b89ac
manosetro <manosetro@iti.gr>
2017-11-15 17:41:44 +0200

    Add auto cache in web service api

---
+++ web-service/auto_cache.py
@@ -0,0 +1,42 @@
+import requests
+import time
+import datetime
+import sys
+
+ip = sys.argv[0]
+service = 'http://{}/api'.format(ip)
+collectionsUrl = '{}/collections'.format(service)
+
+time.sleep(300)
+
+while True:
+    t1 = time.time()
+    r = requests.get(collectionsUrl)
+    collections = r.json()
+
+    owners = dict()
+    for collection in collections:
+        owner = collection['ownerId']
+        owners[owner] = owners.get(owner, 0) + 1
+
+    print('%s:  %s collections found from %s owners' % (datetime.datetime.fromtimestamp(t1).strftime('%Y-%m-%d %H:%M:%S'), len(collections), len(owners)))
+    for owner in owners:
+        count = owners[owner]
+        try:
+            ownerCollectionsUrl = '{}/collection/{}?cached=false&nPerPage={}&pageNumber=1'.format(service, owner, count)
+            r = requests.get(ownerCollectionsUrl)
+        except IOError as e:
+            print("I/O error({0}): {1}".format(e.errno, e.strerror))
+
+    dt = time.time() - t1
+    if dt < 180:
+        sleep_time = 180 - dt
+        print('Sleep time: %s' % sleep_time)
+        try:
+            time.sleep(sleep_time)
+        except KeyboardInterrupt:
+            # exit script gracefully while running
+            sys.exit()
+        except Exception:
+            # handle exceptions on a general level"
+            print("Exception during sleep time")
--- web-service/run.sh
+++ web-service/run.sh
@@ -5,6 +5,11 @@ cd /var/www/site/api
 composer install
 cd /
 
+#start memcached
 service memcached start
 
+#start auto-caching
+python auto_cache.py localhost &
+
+#start Apache
 /usr/sbin/apache2ctl -D FOREGROUND
